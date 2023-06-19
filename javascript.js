477fdc2c9f6a7bb15678e44260aa587329ef8236
majd <majd.teffeha@gmail.com>
2023-06-19 23:09:23 +0300

    created two functions for the game

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function getComputerChoice() {
+    let choice= ["rock","paper","scissors"];
+    let randomchoice= choice[Math.floor(Math.random()*choice.length)];
+    return randomchoice
+}
+function playround(playerSelection,computerSelection) {
+    playerSelection="rock";
+    computerSelection=getComputerChoice();
+    let declaration;
+    if (playerSelection==computerSelection){
+        declaration="it's a draw";
+    }
+    else {
+        if (playerSelection=="rock" && computerSelection=="scissors"){
+            declaration="you win! rock beats scissors"
+        }
+        else if (playerSelection=="scissors"&& computerSelection=="rock"){
+            declaration="you lose! rock beats scissors"
+        }
+        else if (playerSelection=="rock"&& computerSelection=="paper"){
+            declaration="you lose! paper beats rock"
+        }
+        else if (playerSelection=="paper"&& computerSelection=="rock"){
+            declaration="you win! paper beats rock"
+        }
+        else if (playerSelection=="scissors"&& computerSelection=="paper"){
+            declaration="you win! scissors beats paper"
+        }
+        else if (playerSelection=="paper"&& computerSelection=="scissors"){
+            declaration="you lose! scissors beats paper"
+        }
+    }
+return declaration
+    }
+    
